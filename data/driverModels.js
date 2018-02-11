114e20004483e3ef7a1dce0594dd57101f1dabf3
Derrick Zhang <dcheung41@gmail.com>
2018-02-10 21:45:27 -0800

    updated server and driver models with proper queries, set up SQS with 3 queues to accept driver sign ons, sign offs, and driver matches, http request for driver match, and a cron job to send total driver counts per city

--- data/driverModels.js
+++ data/driverModels.js
@@ -18,7 +18,8 @@ const DriverSchema = new Schema({
 		type: Number
 	},
 	city: {
-		type: String
+		type: String,
+		required: true
 	},
 	currentLocation: GeoSchema 
 })
@@ -37,58 +38,185 @@ const Location = mongoose.model('driver', DriverSchema);
 const Count = mongoose.model('driverCount', DriverCountSchema);
 
 let findNearestDriver = (longitude,latitude) => {
-	Location.aggregate().near({
+	return Location.aggregate().near({
 		near: {
-			'type': 'Point',
+			'type': 'point',
 			'coordinates': [parseFloat(longitude),parseFloat(latitude)]
 			},
-		maxDistance: 1000,
+		maxDistance: 500,
 		spherical: true,
 		distanceField: "dis"
 		}).limit(1)
 		.then(result=>{
-			updateDriverCount(result[0].city, -1);
-			removeById(result[0]._id);
-			console.log('matched driver', result)
+			// updateDriverCount(result[0].city, -1);
+			// removeById(result[0]._id);
+			console.log('match driver to user', result)
+			return result;
 		}).catch(err=>{
-			console.log('broken',err)
+			return err;
 		})
 }
 
 
-let addDriver = (context) => {
-	Location.create(context)
-	.then(result => {
-		console.log(result);
-	}).catch(err =>{
-		console.log(err);
+let addDriver = (requestBody) => {
+	// let driver = ctx.request.body
+	console.log('this is the ctx request body', requestBody)
+	return Location.create(requestBody)
+	.catch(err => {
+		throw err;
+	}).then(result =>{
+		return result;
 	})
 }
 
-let updateDriverCount = (currentCity,updateCount) => {
-	Count.update({city:currentCity},{$inc:{count:updateCount}})
+let updateDriverCount = (city,updateCount) => {
+	return Count.update({city:city},{$inc:{count:updateCount}})
 	.then(count =>{
-		console.log('completed count: ',count);
+		return count;
 	}).catch(err=>{
-		console.log('uh o no good',err);
+		console.log('updateDriverCount',err);
+		throw err;
 	})
 }
 
-let removeById = (driverId) => {
-	Location.findByIdAndRemove({_id:driverId})
+let removeById = (id) => {
+	return Location.findByIdAndRemove({_id:id})
 	.then(result =>{
-		console.log('deleted');
+		console.log('confirmed to be found by id and deleted without error', result)
+		return result;
 	}).catch(err =>{
-		console.log(err);
+		throw err;
 	});
 }
 
+//cron job
+let citiesTotalCount = () => {
+	return Count.find()
+	.then(result => {
+		// console.log('found total city count', result);
+		return result;
+	}).catch (err => {
+		return err;
+	})
+}
 
+let getCount = (city) => {
+	return Count.find({city:city})
+	.catch(err => {
+		throw err
+	}).then(result => {
+		return result 
+	})
+}
+
+module.exports.citiesTotalCount = citiesTotalCount; 
 module.exports.findNearestDriver = findNearestDriver;
 module.exports.location = Location; 
 module.exports.count = Count;
 module.exports.addDriver = addDriver;
 module.exports.updateDriverCount = updateDriverCount;
 module.exports.removeById = removeById; 
+module.exports.getCount = getCount; 
+
+// alternatve way to remove
+
+// let removeById = (id) => {
+// 	Location.remove({_id:id})
+// 	.then(result => {
+// 		console.log(result);
+// 		return result;
+// 	}).catch(err => {
+// 		return err;
+// 	})
+// }
+
+// let removeById = (ctx) => {
+// 	let driverId = ctx.request.body._id;
+// 	Location.findByIdAndRemove({_id:driverId})
+// 	.then(result =>{
+// 		updateDriverCount(result.city, -1)
+// 		return result;
+// 	}).catch(err =>{
+// 		return err;
+// 	});
+// }
+
+// let findNearestDriver = (longitude,latitude) => {
+// 	Location.aggregate().near({
+// 		near: {
+// 			'type': 'Point',
+// 			'coordinates': [parseFloat(longitude),parseFloat(latitude)]
+// 			},
+// 		maxDistance: 2000,
+// 		spherical: true,
+// 		distanceField: "dis"
+// 		}).limit(1)
+// 		.then(result=>{
+// 			updateDriverCount(result[0].city, -1);
+// 			removeById(result[0]._id);
+// 			console.log('match driver to user', result)
+// 			return result;
+// 		}).catch(err=>{
+// 			return err;
+// 		})
+// }
+
+//during initialize, run this to get count of every city for 10M
+// let driverCount = () => {
+// 	let cityKeys = Object.keys(datageneration.coordinates)
+// 	cityKeys.forEach((city)=>{
+// 		let closureCity = city;
+// 		Driver.location.find({city:closureCity}).count()
+// 		.then(count=>{
+// 			console.log(closureCity+';'+count);
+// 			Driver.count.create({city:closureCity, count:count})
+// 			.then(result=>{
+// 				console.log('completed for', result)
+// 			}).catch(err =>{
+// 				console.log('it broke');
+// 			})
+// 		}).catch(err => {
+// 			console.log(err);
+// 		})
+// 	})
+// }
+
+//confirmed to work through server
+
+//post request takes an object for body
+// router.post('/driver', (ctx) => {
+// 	Driver.location.create(ctx.request.body)
+// 	.then(result => {
+// 		Driver.count.update({city:result.city},{$inc:{count:1}})
+// 		.then(count=>{
+// 			console.log('completed');
+// 		}).catch(err=>{
+// 			console.log('uh o no good');
+// 		})
+// 	}).catch(err => {
+// 		console.log(err);
+// 	});
+// })
+
+//get request takes in a query string with lng and lat 
+// router.get('/driver', (ctx) => {
+// 	Driver.location.aggregate().near({
+//       near: {
+//       'type': 'Point',
+//       'coordinates': [parseFloat(ctx.query.lng),parseFloat(ctx.query.lat)]
+//       },
+//       maxDistance: 1000,
+//       spherical: true,
+//       distanceField: "dis"
+//      })﻿.limit(1).then(result=>{
+//      	Driver.count.update({city:result.city},{$inc:{count:-1}})
+//      	Driver.findByIdAndRemove({_id: result[0]._id})
+//      	.then(result=>{
+//      		console.log('deleted');
+//      	})
+//      }).catch(err =>{
+//      	console.log(err);
+//      });
+// })
 
 
--- server/mongoIndex.js
+++ server/mongoIndex.js
@@ -1,16 +1,116 @@
-let async = require('async');
+require('newrelic');
 let Koa = require('koa');
 let bodyParser = require('koa-bodyparser');
 let Router = require('koa-router');
-let fs = require('fs');
 let mongoose = require('mongoose');
-let datageneration = require('../data/datageneration.js');
-
 let Driver = require('../data/driverModels.js');
+let AWS = require('aws-sdk');
+let Consumer = require('sqs-consumer');
+let access = require('../config.json');
+let cron = require('node-cron');
+let path = require('path');
 
+AWS.config.update({
+  region: access.region,
+  accessKeyId: access.accessKeyId,
+  secretAccessKey: access.secretAccessKey
+});
+//AWS SQS
+let sqs = new AWS.SQS({apiVersion: "2012-11-05"});
+// AWS.config.loadFromPath(path.resolve(__dirname, '../config.json'));
+
+let queueURLS = {
+	driverMatch : 'https://sqs.us-west-1.amazonaws.com/642265802339/driverMatch',
+	driverSignOn : 'https://sqs.us-west-1.amazonaws.com/642265802339/driverSignOn',
+	driverSignOff : 'https://sqs.us-west-1.amazonaws.com/642265802339/driverSignOff', 
+	sendDriverCount : "https://sqs.us-west-1.amazonaws.com/278687533626/drivers"
+}
+
+// example test { "pickUpLocation": [-97.72, 30.25] }
+const driverMatch = Consumer.create({
+	// queueUrl: 'https://sqs.us-west-1.amazonaws.com/642265802339/driverMatch',
+	queueUrl: queueURLS.driverMatch,
+	handleMessage: async (message, done) => {
+		let incomingMessage = JSON.parse(message.Body);
+		let longitude = incomingMessage.pickUpLocation[0]
+		let latitude = incomingMessage.pickUpLocation[1]
+		let closestDriver = await Driver.findNearestDriver(longitude,latitude);
+		Driver.updateDriverCount(closestDriver[0].city, -1);
+		Driver.removeById(closestDriver[0]._id)
+		done();
+	}
+});
+//example test {"city" : "denver", "currentLocation" : { "type" : "Point", "coordinates" : [ -104.88978433615523, 39.72585861269949 ]  }}
+const driverSignOn = Consumer.create({
+	// queueUrl: 'https://sqs.us-west-1.amazonaws.com/642265802339/driverSignOn',
+	queueUrl: queueURLS.driverSignOn,
+	handleMessage: (message, done) => {
+		let incomingMessage = JSON.parse(message.Body);
+		console.log('incomingMessage', incomingMessage)
+		Driver.addDriver(incomingMessage);
+		Driver.updateDriverCount(incomingMessage.city, 1);
+		done();
+	}
+});
+//example test {  "_id" : "5a7a26978e7beb4d43d673da", "city" : "denver", "currentLocation" : { "type" : "Point", "coordinates" : [ -104.88978433615523, 39.72585861269949 ]  } }
+const driverSignOff = Consumer.create({
+    // queueUrl: 'https://sqs.us-west-1.amazonaws.com/642265802339/driverSignOff',
+    queueUrl: queueURLS.driverSignOff, 
+    handleMessage: (message, done) => {
+    	let incomingMessage = JSON.parse(message.Body);
+    	console.log('parse', incomingMessage._id);
+    	Driver.removeById(incomingMessage._id);
+    	Driver.updateDriverCount(incomingMessage.city);
+     done();
+ 	}
+ })
+//initiliaze queue and listener for driverMatch, driverSignon, and driverSignOff and error handle
+driverMatch.on('error', (err) => {
+  console.log(err.message);
+});
+driverSignOn.on('error', (err) => {
+  console.log(err.message);
+});
+driverSignOff.on('error', (err) => {
+  console.log(err.message);
+})
+//initialize queues for driverMatch, driverSignOn, and driverSignOff
+driverMatch.start();
+driverSignOn.start();
+driverSignOff.start();
+
+// var sendCityCounts = async (msgBody, queueUrl) => {
+//   let citiesCount = await msgBody()
+//   var params = {
+//     MessageBody: JSON.stringify(citiesCount),
+//     QueueUrl: queueUrl,
+//     DelaySeconds: 0
+//   };
+//     sqs.sendMessage(params, function(err, data) {
+//       if(err) {
+//       	console.log(err)
+//       	return err;
+//       }
+//       else {
+//         console.log('received data',data);
+//         return data; 
+//       }  
+//   });
+// };
+
+//CRON JOB FOR DEEEEE CROSSSSS, sending driver count to Derrick
+// cron.schedule('* */1 * * *', function(){
+//   console.log('running a task every hour');
+//   sendCityCounts(Driver.citiesTotalCount, queueURLS.sendDriverCount); 
+// });
+
+
+//initializing Koa and running routes ---------------
 let app = new Koa();
 let router = new Router(); 
+app.use(router.routes())
 
+//connecting to MongoDB with uber as our DB -----------------
 mongoose.connect('mongodb://localhost/uber', function(err, db) {
     if (err) {
         console.log('Unable to connect to the server. Please start the server. Error:', err);
@@ -20,101 +120,115 @@ mongoose.connect('mongodb://localhost/uber', function(err, db) {
 });
 
 mongoose.Promise = global.Promise;
-
+//Using middlewear such as body parser
 router.use(bodyParser());
-app.use(router.routes())
 
+//Initilizing server, listening on port 4000
 app.listen(process.env.port || 4000, () => { 
 	console.log("Koa is listening on port 4000");
 });
 
-//get request takes in a query string with lng and lat 
+
+router.get('/drivercount/:city', async (ctx) => {
+	console.log(ctx.params.city);
+	try {
+		let getCount = await Driver.getCount(ctx.params.city)
+		ctx.body = getCount[0].count;
+		ctx.status = 200;
+	} catch (err) {
+		ctx.status = 404;
+		return err
+	}
+})
+
+router.post('/driver', async (ctx) => {
+	try {
+		let add = Driver.addDriver(ctx.request.body);
+		let updateCount = Driver.updateDriverCount(ctx.request.body.city,1);
+		let result = await Promise.all([add, updateCount]);
+		ctx.status = 201;
+	} catch (err) {
+		ctx.status = 404;
+		return err;
+	}
+})
+
+router.delete('/driver', async (ctx) => {
+	console.log(ctx.request.body, 'kjl;adsfljadlsfadflsaldflsa;dfsadflsals;fdl;s')
+	try {
+		let removeId = Driver.removeById(ctx.request.body._id);
+		let updateCount = Driver.updateDriverCount(ctx.request.body.city, -1);
+		let result = await Promise.all([removeId, updateCount]);
+		// let updateCount = Driver.updateDriverCount(ctx,-1);
+		// let result = await Promise.all([remove, updateCount]);
+		ctx.status = 200;
+	} catch (err) {
+		ctx.status = 404;
+		return err;
+	}
+})
+
+module.exports = app;
+
+//get request takes in a query string with lng and lat, ride matches driver/user, deletes driver, and updates city count by decrementing 1
 	//example string query would be http://localhost:4000/driver/?lng=-122.5&lat=37.7
-router.get('/driver', (ctx) => {
-	longitude = ctx.query.lng;
-	latitude = ctx.query.lat;
-	Driver.findNearestDriver(longitude,latitude);
-})
+// router.get('/driver', async (ctx) => {
+// 	try {
+// 		let longitude = ctx.query.lng;
+// 		let latitude = ctx.query.lat;
+// 		let closestDriver = await Driver.findNearestDriver(longitude,latitude);
+// 		ctx.body = closestDriver;
+// 		// console.log('found driver and matched');
+// 		// let updateCount = Driver.updateDriverCount(closestDriver[0].city,-1); 
+// 		Driver.removeById(closestDriver[0]._id);
+// 		// console.log('deleted specific id after matching driver to user')
+// 		// let result = await Promise.all([updateCount,removeId]);
+// 		ctx.status = 200; 
+// 		// console.log('anything in the body',closestDriver);
+// 	} catch (err) {
+// 		console.log(err)
+// 		ctx.status = 404;
+// 	}
+// })
 
-router.get('/driver', (ctx) => {
-	let driver = ctx.request.body;
-	Driver.updateDriverCount(driver.city,-1)
-})
-
-
-//post request takes an object for body
-router.post('/driver', (ctx) => {
-	let driver = ctx.request.body;
-	Driver.addDriver(driver);
-	Driver.updateDriverCount(driver.city,1);
-})
-
+// post request takes an object for body
+// 	posts driver into driver db and decrements one from drivercount db.
 //delete takes in a request object 
-router.delete('/driver/', (ctx) => {
-	let driver = ctx.request.body;
-	console.log(driver._id, driver.city);
-	Driver.removeById(driver._id);
-	Driver.updateDriverCount(driver.city,-1)
-})
-
-//during initialize, run this to get count of every city for 10M
-// let driverCount = () => {
-// 	let cityKeys = Object.keys(datageneration.coordinates)
-// 	cityKeys.forEach((city)=>{
-// 		let closureCity = city;
-// 		Driver.location.find({city:closureCity}).count()
-// 		.then(count=>{
-// 			console.log(closureCity+';'+count);
-// 			Driver.count.create({city:closureCity, count:count})
-// 			.then(result=>{
-// 				console.log('completed for', result)
-// 			}).catch(err =>{
-// 				console.log('it broke');
-// 			})
-// 		}).catch(err => {
-// 			console.log(err);
-// 		})
-// 	})
-// }
+	//removes driver completley from driver db and decrements ne from drivercount db. 
 
 
-//confirmed to work through server
+//// SQS THAT TAKES IN ALL MESSAGES
 
-//post request takes an object for body
-// router.post('/driver', (ctx) => {
-// 	Driver.location.create(ctx.request.body)
-// 	.then(result => {
-// 		Driver.count.update({city:result.city},{$inc:{count:1}})
-// 		.then(count=>{
-// 			console.log('completed');
-// 		}).catch(err=>{
-// 			console.log('uh o no good');
-// 		})
-// 	}).catch(err => {
-// 		console.log(err);
-// 	});
-// })
+// const DriverTrackingService = Consumer.create({
+//   queueUrl: 'https://sqs.us-west-1.amazonaws.com/642265802339/DriverTrackingService',
+//   handleMessage: async (message, done) => {
+//     let incomingMessage = JSON.parse(message.Body);
+//     //if pickuplocation exists, user is looking for driver to match. 
+//     //get from albert
+//     if(incomingMessage.pickUpLocation){
+// 		let longitude = incomingMessage.pickUpLocation[0]
+// 		let latitude = incomingMessage.pickUpLocation[1]
+// 		let closestDriver = await Driver.findNearestDriver(longitude,latitude);
+// 		Driver.updateDriverCount(closestDriver[0].city, -1);
+// 		Driver.removeById(closestDriver[0]._id)
+// 	//delete the driver, get rid from db
+//     } else if (incomingMessage._id){
+//     	Driver.removeById(incomingMessage._id)
+//     	Driver.updateDriverCount(incomingMessage.city, -1)
+//     //user signs on, add to db
+//     } else if (incomingMessage.city){
+//     	console.log('trying to add driver');
+//     	Driver.addDriver(incomingMessage);
+//     	Driver.updateDriverCount(incomingMessage.city, 1);
+//     }
+//     done();
+//   }
+// });
+// DriverTrackingService.on('error', (err) => {
+//   console.log(err.message);
+// });
+// DriverTrackingService.start();
 
-//get request takes in a query string with lng and lat 
-// router.get('/driver', (ctx) => {
-// 	Driver.location.aggregate().near({
-//       near: {
-//       'type': 'Point',
-//       'coordinates': [parseFloat(ctx.query.lng),parseFloat(ctx.query.lat)]
-//       },
-//       maxDistance: 1000,
-//       spherical: true,
-//       distanceField: "dis"
-//      })﻿.limit(1).then(result=>{
-//      	Driver.count.update({city:result.city},{$inc:{count:-1}})
-//      	Driver.findByIdAndRemove({_id: result[0]._id})
-//      	.then(result=>{
-//      		console.log('deleted');
-//      	})
-//      }).catch(err =>{
-//      	console.log(err);
-//      });
-// })
 
 
 
